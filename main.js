eb49afc7c64d0211c112c0f6f9b180f97dd04d2e
imsangwoo <asig1352@gmail.com>
2020-10-24 21:53:36 +0900

    JS : Scroll to section

--- main.js
+++ main.js
@@ -10,3 +10,19 @@ document.addEventListener('scroll',() => {
     navbar.classList.remove('navBar')
   }
 })
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click',(event) => {
+  // console.log(event.target.dataset.link);
+
+  const tarGet = event.target;
+  const link = tarGet.dataset.link;
+
+  if(link === null) {
+    return;
+  } 
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+  
+})
