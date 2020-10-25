a2b45644f50581a279e0861271bb932073820208
imsangwoo <asig1352@gmail.com>
2020-10-25 16:00:10 +0900

    JS : transparent home

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const navbar = document.querySelector('#navbar');
+const home = document.querySelector('#home');
 const navBarheight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll',() => {
@@ -12,17 +13,30 @@ document.addEventListener('scroll',() => {
 })
 
 const navbarMenu = document.querySelector('.navbar__menu');
+const homeContent = document.querySelector('.home__contect');
 
-navbarMenu.addEventListener('click',(event) => {
-  // console.log(event.target.dataset.link);
+navbarMenu.addEventListener('click', handleClick)
+homeContent.addEventListener('click', handleClick )
 
-  const tarGet = event.target;
-  const link = tarGet.dataset.link;
+function handleClick(event) {
+    const tarGet = event.target;
+    const link = tarGet.dataset.link;
+    if(link === null) {
+      return;
+    }
+    scrollintoview(link);
+}
 
-  if(link === null) {
-    return;
-  } 
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-  
+const homeContainer = document.querySelector('.home__container');
+const homecontainerHeight = homeContainer.getBoundingClientRect().height;
+
+document.addEventListener('scroll',() => {
+  homeContainer.style.opacity = 1 - window.scrollY / homecontainerHeight;
 })
+
+function scrollintoview(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth" , block:"center"});
+}
+
+
